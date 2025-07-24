8d7de91f87e64b935f1999e259390d35a141f161
vishalshingate <vishalshingate26@gmail.com>
2025-07-24 13:14:48 +0530

    first commit

---
+++ src/main/java/com/example/uberprojectentity/UberProjectEntityApplication.java
@@ -0,0 +1,13 @@
+package com.example.uberprojectentity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UberProjectEntityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UberProjectEntityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/uberprojectentity/UberProjectEntityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uberprojectentity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UberProjectEntityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
